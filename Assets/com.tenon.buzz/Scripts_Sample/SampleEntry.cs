2cedfc2bd8791175546dded4f5e0234864fb7f66
onovich <hjn1110@163.com>
2024-05-26 18:31:07 +0800

    <sample> add: Sample

--- Assets/com.tenon.buzz/Scripts_Sample/SampleEntry.cs
+++ Assets/com.tenon.buzz/Scripts_Sample/SampleEntry.cs
@@ -1,17 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-namespace TenonKit.Buzz {
+public class SampleEntry : MonoBehaviour {
+    private Gamepad gamepad;
 
-    public class SampleEntry : MonoBehaviour {
-        void Start() {
+    void Start() {
+        TrySetCurrentGamepad();
+    }
 
-        }
+    bool TrySetCurrentGamepad() {
+        if (Gamepad.current != null) {
+            gamepad = Gamepad.current;
+            Debug.Log("手柄已连接");
 
-        void Update() {
-
+            // 测试手柄是否支持震动
+            try {
+                gamepad.SetMotorSpeeds(0.1f, 0.1f);
+                Debug.Log("手柄支持震动");
+                gamepad.SetMotorSpeeds(0, 0); // 停止震动
+                return true;
+            } catch (System.Exception) {
+                Debug.Log("手柄不支持震动");
+                return false;
+            }
+        } else {
+            Debug.Log("没有连接手柄");
+            return false;
         }
     }
 
+    void Update() {
+        if (gamepad == null) {
+            var succ = TrySetCurrentGamepad();
+            if (!succ) {
+                return;
+            }
+        }
+
+        var lowFreq = 0f;
+        var highFreq = 0f;
+
+        // 按键A，手柄小马达震动，频率低
+        if (gamepad.buttonSouth.wasPressedThisFrame) {
+            lowFreq = 0.25f;
+            // Debug.Log("按键A：小马达低频震动");
+        }
+
+        // 按键B，手柄大马达震动，频率低
+        if (gamepad.buttonEast.wasPressedThisFrame) {
+            highFreq = 0.25f;
+            // Debug.Log("按键B：大马达低频震动");
+        }
+
+        // 按键X，手柄小马达震动，频率高
+        if (gamepad.buttonWest.wasPressedThisFrame) {
+            lowFreq = 1.0f;
+            // Debug.Log("按键X：小马达高频震动");
+        }
+
+        // 按键Y，手柄大马达震动，频率高
+        if (gamepad.buttonNorth.wasPressedThisFrame) {
+            highFreq = 1.0f;
+            // Debug.Log("按键Y：大马达高频震动");
+        }
+
+        if (lowFreq > 0 || highFreq > 0) {
+            gamepad.SetMotorSpeeds(lowFreq, highFreq);
+        }
+
+        // 停止震动
+        if (!gamepad.buttonSouth.isPressed &&
+            !gamepad.buttonEast.isPressed &&
+            !gamepad.buttonWest.isPressed &&
+            !gamepad.buttonNorth.isPressed) {
+            gamepad.SetMotorSpeeds(0, 0);
+        }
+    }
 }
