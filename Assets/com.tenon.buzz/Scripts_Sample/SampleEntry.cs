378d44d4223fdb7c8c464e571ef93cbf15b196f3
onovich <hjn1110@163.com>
2024-05-26 17:15:44 +0800

    ESTABLISH

---
+++ Assets/com.tenon.buzz/Scripts_Sample/SampleEntry.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TenonKit.Buzz {
+
+    public class SampleEntry : MonoBehaviour {
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+    }
+
+}
